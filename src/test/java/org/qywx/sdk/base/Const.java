1ccfd59aeea8ab3042ce0aff4c84ef56c826fd12
Rocye <hirocye@163.com>
2018-02-24 17:34:44 +0800

    Initial commit

---
+++ src/test/java/org/qywx/sdk/base/Const.java
@@ -0,0 +1,16 @@
+package org.qywx.sdk.base;
+
+/**
+ * 测试用常量
+ * @author Rocye
+ * @version 2018.01.22
+ */
+public class Const {
+	//企业微信应用CORPID
+    public static String CORPID = "XXX";
+    //企业微信应用CORPSERCT
+    public static String CORPSERCT = "XXX";
+	//企业微信应用AGENTID
+	public static Long AGENTID = 1000005L;
+
+}
